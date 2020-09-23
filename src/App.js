37e4cac8d1c770a5af2a3ed47eb89dff12407f43
Yohan <gentlebae@gmail.com>
2020-09-23 08:14:58 -0400

    first init

--- src/App.js
+++ src/App.js
@@ -2,23 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import S3FileUpload from 'react-s3';
+
+
 function App() {
+  const config = {
+      bucketName: 'prismagram-hani',
+      dirName: 'photos', /* optional */
+      region: 'us-east-1',
+      accessKeyId: process.env.REACT_APP_ACCESS_KEY_ID,
+      secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_KEY,
+  }
+
+  const onUpload = (e) => {
+    console.log(e.target.files[0], process.env.REACT_APP_ACCESS_KEY_ID)
+    S3FileUpload.uploadFile(e.target.files[0], config)
+      .then(data => console.log(data))
+      .catch(err => console.log(err))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h5>Upload File</h5>
+      <input type="file" onChange={onUpload} />
     </div>
   );
 }
